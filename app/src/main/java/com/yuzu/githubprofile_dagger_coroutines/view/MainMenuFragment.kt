974ebc2a1fa04e292488b0243ce98bdf0dff1794
Yustar <yustar.shooter@gmail.com>
2022-09-09 09:35:16 +0700

    add jetpack navigation component. link navigation to MainMenuFragment.kt

--- app/src/main/java/com/yuzu/githubprofile_dagger_coroutines/view/MainMenuFragment.kt
+++ app/src/main/java/com/yuzu/githubprofile_dagger_coroutines/view/MainMenuFragment.kt
@@ -1,6 +1,22 @@
 package com.yuzu.githubprofile_dagger_coroutines.view
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.yuzu.githubprofile_dagger_coroutines.databinding.FragmentMainMenuBinding
 
 class MainMenuFragment: Fragment() {
+    private lateinit var binding: FragmentMainMenuBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        binding = FragmentMainMenuBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
