e1a203705c4d519033f379d7fadd1cf6880ca499
Yustar <yustar.shooter@gmail.com>
2022-09-09 10:18:05 +0700

    add mock fragment for populr and favorite. test implement viewPager2

--- app/src/main/java/com/yuzu/githubprofile_dagger_coroutines/view/MainMenuFragment.kt
+++ app/src/main/java/com/yuzu/githubprofile_dagger_coroutines/view/MainMenuFragment.kt
@@ -5,18 +5,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.material.tabs.TabLayoutMediator
 import com.yuzu.githubprofile_dagger_coroutines.databinding.FragmentMainMenuBinding
 
 class MainMenuFragment: Fragment() {
     private lateinit var binding: FragmentMainMenuBinding
+    private val title = arrayOf("Popular", "Favorite")
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+                              savedInstanceState: Bundle?): View {
         binding = FragmentMainMenuBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.viewPager.adapter = ViewPagerAdapter(childFragmentManager, lifecycle)
+
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+            tab.text = title[position]
+        }.attach()
     }
 }
