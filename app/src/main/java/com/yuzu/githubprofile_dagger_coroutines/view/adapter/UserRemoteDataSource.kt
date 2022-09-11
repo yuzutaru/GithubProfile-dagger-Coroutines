f84279f1673d19cf94b2e6d33e68a19984e9371e
Yustar <yustar.shooter@gmail.com>
2022-09-11 09:32:33 +0700

    popular fragment margin adjustment, view model clean up code

--- app/src/main/java/com/yuzu/githubprofile_dagger_coroutines/view/adapter/UserRemoteDataSource.kt
+++
@@ -1,9 +0,0 @@
-package com.yuzu.githubprofile_dagger_coroutines.view.adapter
-
-import androidx.paging.PagingData
-import com.yuzu.githubprofile_dagger_coroutines.repository.data.User
-import kotlinx.coroutines.flow.Flow
-
-interface UserRemoteDataSource {
-    suspend fun getPopularUser(): Flow<PagingData<User>>
-}
--- app/src/main/java/com/yuzu/githubprofile_dagger_coroutines/viewmodel/PopularViewModel.kt
+++ app/src/main/java/com/yuzu/githubprofile_dagger_coroutines/viewmodel/PopularViewModel.kt
@@ -3,16 +3,10 @@ package com.yuzu.githubprofile_dagger_coroutines.viewmodel
 import androidx.lifecycle.*
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
-import androidx.paging.PagingData
 import androidx.paging.liveData
 import com.yuzu.githubprofile_dagger_coroutines.GithubApp
-import com.yuzu.githubprofile_dagger_coroutines.repository.data.Resource
-import com.yuzu.githubprofile_dagger_coroutines.repository.data.User
 import com.yuzu.githubprofile_dagger_coroutines.repository.remote.contract.ProfileRepository
-import com.yuzu.githubprofile_dagger_coroutines.view.adapter.NETWORK_PAGE_SIZE
 import com.yuzu.githubprofile_dagger_coroutines.view.adapter.UserPagingSource
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.Flow
 
 class PopularViewModel: ViewModel() {
     private val profileRepository: ProfileRepository
@@ -29,7 +23,7 @@ class PopularViewModel: ViewModel() {
 
     var userLiveData = user.switchMap {
         Pager(
-            config = PagingConfig(enablePlaceholders = false, pageSize = NETWORK_PAGE_SIZE),
+            config = PagingConfig(enablePlaceholders = false, pageSize = 10),
             pagingSourceFactory = { UserPagingSource(profileRepository) }
         ).liveData
     }
